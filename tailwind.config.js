3033ea27a08dc434d5271f1499d2a9c58ec0b19c
mhdhaikalll <atokzzz04@gmail.com>
2025-12-21 00:22:23 +0800

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.export = {
+    content: [
+        "./*.html",
+        "./pages/*.html",
+        "./components/*.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
